b799a6a510be94d00c4f0f83813079e91499b887
llqhz <llqhz@qq.com>
2018-05-18 11:31:34 +0800

    commit at 2018-05-18 05:31:33

--- commit.php
+++ commit.php
@@ -3,12 +3,12 @@
  * @Author: Marte
  * @Date:   2018-05-18 11:25:13
  * @Last Modified by:   Marte
- * @Last Modified time: 2018-05-18 11:29:05
+ * @Last Modified time: 2018-05-18 11:31:19
  */
 $step_1 = 'git add .';
 $step_2 = 'git commit -m " commit at '.date('Y-m-d H:i:s').' "';
 $step_3 = 'git push origin';
 shell_exec($step_1);
 shell_exec($step_2);
-$res = shell_exec($step_3);
-var_dump($res);
+shell_exec($step_3);
+
