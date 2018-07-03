860cc02a00c66492b2a5dd2f2d93c4edce95ef26
☆筱 怪☆ <llqhz@qq.com>
2018-07-03 12:33:11 +0800

    phpqrcode类库
    phpqrcode类库

--- composer.php
+++ composer.php
@@ -32,3 +32,15 @@ windows 安装多版本composer
 找到composer.phar 所在目录 =>  C:\ProgramData\ComposerSetup\bin
 复制composer composer.bat
 改名为composer5 composer5.bat
+ 
+ 
+ 安装类库的坑:
+ (1) 没有注意目录拼写 vendor 而不是 vender
+ (2) path_info 配置中: input('') => $_GET 只有input才能获取path_info变量
+     当框架可以正常解析模块控制器方法时,path_info就已经配置成功了
+ (3) 注意需要更改和编写的文件 , 不可以编写这个而实际上运行的是另一个文件
+ (4) phpqrcode内类库需要自己vendor('path/to/qrlib','.php') 起始目录为'vendor/'
+ (5) composer.json中 autoload => { 'classmap'=> [],'files'=>[] } 的起始目录为 'ROOT_PATH',需要自己加vendor => eg: 'vendor/phpqrcode/phpqrcode';
+ (6) phpqrcode 没有创建cache文件夹的权限,需要自己在src文件夹建好cache文件夹
+ (7) think_captcha 只要加载成功就会正常运行
+ 
