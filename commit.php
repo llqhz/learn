c141b578df1e7e0dc25776c9bbd81a5e4c97cc30
筱怪 <llqhz@qq.com>
2019-07-22 00:36:02 +0800

    测试分支修改

--- commit.php
+++ commit.php
@@ -8,6 +8,7 @@
 $step_1 = 'git add .';
 $step_2 = 'git commit -m " commit at '.date('Y-m-d H:i:s').' "';
 $step_3 = 'git push origin';
+
 shell_exec($step_1);
 shell_exec($step_2);
 shell_exec($step_3);
